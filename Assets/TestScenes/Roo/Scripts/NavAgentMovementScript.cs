e00a388e2d06ae2b1d0855d2f1c25d68bb64f51d
Roo <roo.kooistra@gmail.com>
2019-06-30 22:35:15 +1000

    Testing controlling multiple explorers at once.

---
+++ Assets/TestScenes/Roo/Scripts/NavAgentMovementScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentMovementScript : MonoBehaviour
+{
+    private NavMeshAgent _explorer;
+
+    private bool _isActive = false;
+    private Renderer _rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _explorer = GetComponent<NavMeshAgent>();
+        _rend = GetComponent<Renderer>();
+        _rend.material.color = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.collider.gameObject == this.gameObject)
+                {
+                    _isActive = !_isActive;
+                    if (_isActive) { _rend.material.color = Color.green; }
+                    else
+                    {
+                        _rend.material.color = Color.black;
+                        _explorer.isStopped = true;
+                    }
+                        
+
+                }
+                else
+                    if (_isActive && hit.collider.tag != "Explorer") { _explorer.destination = hit.point; }
+                
+            }
+        }
+    }
+}
