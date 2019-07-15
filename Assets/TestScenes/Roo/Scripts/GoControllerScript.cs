270808e8db3f3e458b85061a49aed498e2f8b609
Roo <roo.kooistra@gmail.com>
2019-07-15 17:05:16 +1000

    Added copy of main scene or go controller testing. added controller checks... (to be left , right hand specific)

--- Assets/TestScenes/Roo/Scripts/GoControllerScript.cs
+++ Assets/TestScenes/Roo/Scripts/GoControllerScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class GoControllerScript : MonoBehaviour
 {
-
+    LineRenderer _line;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _line = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool controllerCheck = OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote) || OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote);
+
+        if (!controllerCheck)
+        {
+            _line.enabled = false;
+            return;
+        }
+        _line.enabled = true;
         transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
     }
 }
