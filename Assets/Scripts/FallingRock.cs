c664682f4540c79ee7f8aaecc614a3c964d641bd
murphy <doyle>
2019-07-15 11:05:02 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/FallingRock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingRock : MonoBehaviour
+{
+    public GameObject Rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Explorer"))
+        {
+            Rigidbody RB = Rock.GetComponent<Rigidbody>();
+
+            RB.useGravity = true;
+        }
+           
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform Exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit something");
+        if (other.gameObject.CompareTag("Explorer"))
+        {
+            Debug.Log("Hit explorer");
+            other.transform.position = Exit.position;
+        }
+    }
+}
