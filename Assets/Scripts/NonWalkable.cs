72e834acfe768532926baa29b3d07240467dd0c8
murphy <1009894@student.sae.edu.au>
2019-07-16 10:13:25 +0800

    Fixing hit boxes for events

---
+++ Assets/Scripts/NonWalkable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonWalkable : MonoBehaviour
+{
+    public GameObject BoxBoi;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Explorer"))
+        {
+            BoxBoi.SetActive(true);
+        }
+    }
+}
