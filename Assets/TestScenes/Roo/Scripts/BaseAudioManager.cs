83927dd9dcd66d16e83e4ba3eacadf2839b676c9
Roo <roo.kooistra@gmail.com>
2019-08-05 10:49:06 +1000

    Atmos and SFX building

--- Assets/TestScenes/Roo/Scripts/BaseAudioManager.cs
+++ Assets/TestScenes/Roo/Scripts/BaseAudioManager.cs
@@ -23,33 +23,17 @@ public class BaseAudioManager : MonoBehaviour
      *                                                                                                  *
      ***************************************************************************************************/
 
-    /*
-     *  BELOW IS A SAMPLE, CHANGE FOR CORRECT NAMES
-     *  
-    public static FMOD.Studio.EventInstance score1, score2, score3; // Game sounds from Score bank
-    public static FMOD.Studio.EventInstance atmos1, atmos2, atmos3; // Game sounds from Atmos bank
-    */
-
-    /*
-     * 
-     * 
-     *                     UNCOMMENT BELOW SECTION AND CHANGE ACCORDINGLY
-     *                     
-     *                     
-     *                     
-     *                     
-     *                     
-   public static FMOD.Studio.EventInstance Exploring, Struggle; // Game sounds from atmos bank
-
-
+   
+   public static FMOD.Studio.EventInstance volcanoAtmos, oceanAtmos; // atmos from Score bank
+                       
    // below is examples of links to parameters inside of an eventinstance
    // name the parameters for wind variables
-   public static FMOD.Studio.ParameterInstance WindIntensity; // name the parameter for wind intensity
-   public static FMOD.Studio.ParameterInstance WindVol; // name the parameter for wind Volume
+   // public static FMOD.Studio.ParameterInstance WindIntensity; // name the parameter for wind intensity
+   // public static FMOD.Studio.ParameterInstance WindVol; // name the parameter for wind Volume
 
 
    // Keep music rolling between scenes. Will not destroy onload unless there is a double.
-   static AudioManagerScript instance = null;
+   static BaseAudioManager instance = null;
 
    void Awake()
    {
@@ -69,56 +53,30 @@ public class BaseAudioManager : MonoBehaviour
    {
        // below we connect to all sounds in FMOD for the game
 
-       // sfx bank
-       example1 = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Example1");
-       example2 = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Example2");
+       // atmos bank
+       volcanoAtmos = FMODUnity.RuntimeManager.CreateInstance("event:/Atmos/Burning");
+       // oceanAtmos = FMODUnity.RuntimeManager.CreateInstance("event:/Atmos/Ocean");
 
        // music
        // music = FMODUnity.RuntimeManager.CreateInstance("event:/Score/Music");
 
-       // atmos
-       // atmosExploring = FMODUnity.RuntimeManager.CreateInstance("event:/Atmos/Exploring");
-
-
 
        // connect to sound parameters
 
-       wind.getParameter("WindIntensity", out WindIntensity); // connect to WindIntensity Parameter in Wind Sound
-       wind.getParameter("Strength", out WindVol); // connect to Strength Parameter in Wind Sound and output WindVol
-       music.getParameter("GameLevel", out gameLevel);
+       // wind.getParameter("WindIntensity", out WindIntensity); // connect to WindIntensity Parameter in Wind Sound
+       // wind.getParameter("Strength", out WindVol); // connect to Strength Parameter in Wind Sound and output WindVol
+       // music.getParameter("GameLevel", out gameLevel);
+       
 
-       // keep listing all sounds here
-
-       wind.start(); // start the wind
-       atmosExploring.start(); // start exploring atmos
-   }
-
-   // Update is called once per frame
-   void FixedUpdate()
-   {
-       WindIntensity.setValue(Roo.WindScript.windSpeed);
-       gameLevel.setValue(gameProgression);
+       volcanoAtmos.start(); // start the volcano atmos
+       // atmosExploring.start(); // start exploring atmos
    }
 
    // Below is the switch statement for all the possible sounds used in the game
    public static void Playsound(string clip)
    {
-       switch (clip) { case ("thunder01"): thunder01.start(); break; }
-       switch (clip) { case ("thunder02"): thunder02.start(); break; }
-       switch (clip) { case ("thunder03"): thunder03.start(); break; }
-
-
-       switch (clip) { case ("music"): music.start(); break; }
-       switch (clip) { case ("musicStop"): music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT); break; }
-
-       switch (clip) { case ("windStop"): wind.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT); break; }
-
-       switch (clip) { case ("atmosExploring"): atmosExploring.start(); break; }
-       switch (clip) { case ("atmosStruggle"): atmosStruggle.start(); break; }
-       switch (clip) { case ("atmosExploringStop"): atmosExploring.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT); break; }
-       switch (clip) { case ("atmosStruggleStop"): atmosStruggle.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT); break; }
-
-       //switch (clip) { case ("noWind"): WindIntensity.setValue(0); break; }
+       switch (clip) { case ("startOcean"): oceanAtmos.start(); break; }
+       switch (clip) { case ("stopVolcano"): volcanoAtmos.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT); break; }
    }
-   */
+   
 }
