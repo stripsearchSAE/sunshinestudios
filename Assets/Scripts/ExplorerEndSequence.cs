223429f4f1b979f58e655d8c6596c7ec594576e0
Roo <roo.kooistra@gmail.com>
2019-08-07 11:17:30 +1000

    end sequence code working, explorer childs to platform

--- Assets/Scripts/ExplorerEndSequence.cs
+++ Assets/Scripts/ExplorerEndSequence.cs
@@ -8,23 +8,29 @@ public class ExplorerEndSequence : MonoBehaviour
     public delegate void FinalEvent();
     public static event FinalEvent StartFinalSequence;
 
-    public Vector3 EndingPosition;
-    public ExplorerMovementScript Movement;
+    public Transform endPosition;
+    private ExplorerMovementScript Movement;
     public GameObject Explorer1;
-    public GameObject Explorer2;
+   // public GameObject Explorer2;
     public GameObject Platform;
-    public bool Final;
+    bool Final = false;
+
+    public float distanceCheck = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
 
-
+        Movement = Explorer1.GetComponent<ExplorerMovementScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Vector3.Distance(Explorer1.transform.position, endPosition.position) < distanceCheck && !Final)
+        {
+            Final = true;
+            StartCoroutine(LastPoint());
+        }
     }
 
     public void OnTriggerEnter(Collider other)
@@ -32,23 +38,22 @@ public class ExplorerEndSequence : MonoBehaviour
         if (other.gameObject.CompareTag("Explorer"))
         {
             NavMeshAgent Expl = Movement.explorer;
-            Expl.destination = EndingPosition;
-            StartCoroutine(LastPoint(Expl));
+            Expl.destination = endPosition.position;
+           // StartCoroutine(LastPoint(Expl));
         }
 
     }
 
-    IEnumerator LastPoint(NavMeshAgent a)
+    IEnumerator LastPoint()
     {
-        if (a.pathStatus == NavMeshPathStatus.PathComplete)
-        {
+
             Debug.Log("Howdy");
-            Explorer1.GetComponent<ExplorerMovementScript>().enabled = false;
+            Explorer1.GetComponent<NavMeshAgent>().enabled = false;
             Explorer1.transform.parent = Platform.transform;
-            Explorer2.GetComponent<ExplorerMovementScript>().enabled = false;
-            Explorer2.transform.parent = Platform.transform;
-            if (StartFinalSequence != null) StartFinalSequence();
-        }
+           // Explorer2.GetComponent<ExplorerMovementScript>().enabled = false;
+           // Explorer2.transform.parent = Platform.transform;
+           if (StartFinalSequence != null) StartFinalSequence();
+
         yield return null;
     }
 }
