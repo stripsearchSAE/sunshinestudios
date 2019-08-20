d2cccb4da761b696670c8989eeeb08b8d281b42f
murphy <1009894@student.sae.edu.au>
2019-08-20 12:45:10 +1000

    Setup fade out and reset

---
+++ Assets/Scripts/Fadeout.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fadeout : MonoBehaviour
+{
+    public Image fadebitch;
+    public bool Trig;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Trig == true)
+        {
+
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Explorer"))
+        {
+            fadebitch.CrossFadeAlpha(4f, 4f, false);
+            Trig = true;
+        }
+    }
+}
