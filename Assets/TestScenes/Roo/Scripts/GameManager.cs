83a74d74ad3eadb1b748239d23542dde73ceae2a
Roo <roo.kooistra@gmail.com>
2019-08-21 11:19:20 +1000

    Fade in and out done

--- Assets/TestScenes/Roo/Scripts/GameManager.cs
+++ Assets/TestScenes/Roo/Scripts/GameManager.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public CanvasGroup blackoutCanvas;
 
     public float timeToWaitForReset;
-    public float timeforMusicFadeout = 3.01f;
+    public float fadeInTime;
+    [Header("use Music time for global fadeout")]
+    public float timeForMusicFadeout = 3.01f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(Deactivate(blackoutCanvas, fadeInTime));
         ExplorerEndSequence.StartFinalSequence += StartCountdown;
     }
 
@@ -23,7 +28,8 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(timeToWaitForReset);
         BaseAudioManager.Playsound("stopMusic");
-        yield return new WaitForSeconds(timeforMusicFadeout);
+        StartCoroutine(Activate(blackoutCanvas, timeForMusicFadeout));
+        yield return new WaitForSeconds(timeForMusicFadeout);
         BaseAudioManager.Playsound("stopOcean");
         BaseAudioManager.Playsound("startVolcano");
         BaseAudioManager.Playsound("Level1");
@@ -31,5 +37,31 @@ public class GameManager : MonoBehaviour
         
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator Deactivate(CanvasGroup _panel, float _transitionTime)
+    {
+        while (_panel.alpha > 0)
+        {
+            _panel.alpha -= Time.deltaTime / _transitionTime;
+            yield return null;
+        }
+        _panel.interactable = false;
+        _panel.blocksRaycasts = false;
+
+        yield return null;
+    }
+
+    IEnumerator Activate(CanvasGroup _panel, float _transitionTime)
+    {
+        while (_panel.alpha < 1)
+        {
+            _panel.alpha += Time.deltaTime / _transitionTime;
+            yield return null;
+        }
+        _panel.interactable = true;
+        _panel.blocksRaycasts = true;
+
+        yield return null;
     }
 }
